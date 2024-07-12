5f69bde02bb4366ade8f001ba8a092695420294a
alayna2011 <alaynachia@gmail.com>
2024-07-12 13:14:57 -0700

    Initial Commit

---
+++ Github/GithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubApp.swift
+//  Github
+//
+//  Created by Alayna Yee-Huan Chia on 7/12/24.
+//
+
+import SwiftUI
+
+@main
+struct GithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
